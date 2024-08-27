85a33522f3b58372515f868b438bbdcb27376643
Aakash Deep Yadav <aakashdeepyadav106@gmail.com>
2024-08-28 01:35:17 +0530

    day01q1

--- Day01/q1.cpp
+++ Day01/q1.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main(){
+    string str1, str2, str3;
+    cout << "Enter the first string : " ;
+    cin >> str1 ;    
+    cout << "Enter the second string : " ;
+    cin >> str2 ;
+
+    str3=str1 + str2 ;
+    cout << "Concatenated string:" << str3 ;
     return 0;
 }
