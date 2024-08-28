65da17b0aef7aadc24ab585aec3d88e8139ddbf1
Aakash Deep Yadav <aakashdeepyadav106@gmail.com>
2024-08-28 18:56:01 +0530

    d04q01 added

---
+++ Day04/q1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1, num2;
+    cout << "Enter first integer: ";
+    cin >> num1;
+    cout << "Enter second integer: ";
+    cin >> num2;
+
+    int add, sub, mul, mod;
+    float div;
+    add = num1 + num2;
+    sub = num1 - num2;
+    mul = num1 * num2;
+    while(num2!=0){
+        div = float(num1) / num2;
+    }
+    
+    mod = num1 % num2;
+
+    cout << "Addition: " << add << endl;
+    cout << "Subtraction: " << sub << endl;
+    cout << "Multiplication: " << mul << endl;
+    cout << "Division: " << div << endl;
+    cout << "Modulus: " << mod << endl;
+
+    return 0;
+}
